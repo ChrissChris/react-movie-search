3302457e131d1300d08e7ab9ef659bd776398f7e
Chriss Chris <zenskech@outlook.com>
2020-12-06 21:46:28 +0500

    styling

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -7,8 +7,6 @@ export default function SearchMovies() {
   const searchMovies = async (e) => {
     e.preventDefault();
 
-    const query = "Superman";
-
     const url = `https://api.themoviedb.org/3/search/movie?api_key=af33e7e6a89ca2db62170ccc337eed2d&language=en-US&query=${query}&page=1&include_adult=false`;
 
     try {
@@ -38,15 +36,27 @@ export default function SearchMovies() {
       </form>
 
       <div className="card-list">
-        {movies.map((movie) => (
-          <div className="card">
-            <img
-              className="card--image"
-              src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-              alt={movie.title + " poster"}
-            />
-          </div>
-        ))}
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <div className="card">
+              <img
+                className="card--image"
+                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                alt={movie.title + " poster"}
+              />
+              <div className="card--content">
+                <h3 className="card--title">{movie.title}</h3>
+                <p>
+                  <small>RELEASE DATE: {movie.release_date}</small>
+                </p>
+                <p>
+                  <small>RATING: {movie.vote_average}</small>
+                </p>
+                <p className="card--desc">{movie.overview}</p>
+              </div>
+            </div>
+          ))}
       </div>
     </>
   );
